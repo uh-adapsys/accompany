714aabe3dd54046e9aa567e1b45d8321e02b7a40
Joe <joe@joe-desktop.(none)>
2014-07-18 11:41:11 +0100

    Added option to use ShowMe activities

--- TeachMeShowMe/main.cpp
+++ TeachMeShowMe/main.cpp
@@ -6,6 +6,31 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
+    w.param="";
+
+    opterr = 0;
+    int c;
+
+    while ((c = getopt (argc, argv, "b")) != -1)
+
+    {
+
+      switch (c)
+      {
+         case 'b':
+          w.param = "BATCH";
+            break;
+         default:
+            w.param = "";
+            break;
+      }
+    }
+
+    if (!w.openDatabase())
+    {
+        return 0;
+    }
+
     if (w.closeDownRequest)
     {
         return 0;
